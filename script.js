07f28bb6f6bd5833fd60e39ebac2787ce1532634
Onyedikareal <Frankonyeka25@gmail.com>
2018-07-01 22:54:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/Realo/worker.js').then((registration) => {
         // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log(`ServiceWorker registration successful with scope: ${registration.scope}`);
       }, (err)=> {
         // registration failed :disappointed:
         console.log(`ServiceWorker registration failed:  ${err}`);
